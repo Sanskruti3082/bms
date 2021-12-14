5f840a1a9b8d4a2d9e6d61cdd8d3ce29c4f04b87
Sanskruti3082 <sans.ullagaddi3082@gmail.com>
2021-12-14 16:52:20 +0530

    HC AND LAYOUT

---
+++ src/HOC/Default.HOC.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Route} from "react-router-dom";
+
+//import
+import DefaultLayout from "../layouts/Default.layout";
+
+const DefaultHOC = ({component, ...rest}) => {
+  const Component = component;
+
+  return(
+    <>
+   <Route
+   {...rest}
+   component={(props) => (
+     <DefaultLayout>
+     <Component{...props} />
+     </DefaultLayout>
+   )}
+   />
+    </>
+  );
+};
+
+export default DefaultHOC;
