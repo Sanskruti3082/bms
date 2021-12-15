d36dcbdac114e505acf3c92fcceeeb4fc870367d
Sanskruti3082 <sans.ullagaddi3082@gmail.com>
2021-12-15 18:52:01 +0530

    Navbar comp

--- src/layouts/Default.layout.js
+++ src/layouts/Default.layout.js
@@ -3,7 +3,7 @@ import React from "react";
 const DefaultLayout = (props) => {
   return (
     <div>
-      <h1 className="text-xl">Default Layout</h1>
+      
       {props.children}
     </div>
   );
