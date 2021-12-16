cf4818848c396741bde9f82dd72e3276a7befc90
Sanskruti3082 <sans.ullagaddi3082@gmail.com>
2021-12-16 18:58:44 +0530

    slick

---
+++ src/components/Hero.Carousal/HeroCarousal.component.js
@@ -0,0 +1,39 @@
+import React from "react";
+import HeroSlider from "react-slick";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const HeroCarousal = () => {
+  const settings = {
+    arrows: true,
+    centerMode: true,
+    centerPadding: "150px",
+   dots: true,
+   infinite: true,
+   speed: 500,
+   slidesToShow: 1,
+   slidesToScroll: 1
+ };
+const images = ["https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+"https://images.unsplash.com/photo-1639636669766-4bbd6607239f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80",
+"https://images.unsplash.com/photo-1639610778379-2ee7856f422d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+"https://images.unsplash.com/photo-1639534378945-19b22573681e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+"https://images.unsplash.com/photo-1639502022187-9a4afb389e5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+"https://images.unsplash.com/photo-1639562954961-0aa228b27c62?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+];
+   return (
+     <>
+     <HeroSlider {...settings}>
+     {images.map((image)=>(
+       <div className="w-20 h-80">
+          <img src={image} alt="test-img" className="w-full h-full"/>
+       </div>
+     ))}
+     </HeroSlider>
+     </>
+   )
+};
+
+export default HeroCarousal;
--- src/layouts/Default.layout.js
+++ src/layouts/Default.layout.js
@@ -2,11 +2,14 @@ import React from "react";
 
 
 //Components
-import Navbar from "../components/Navbar/navbar.component.js"
+import Navbar from "../components/Navbar/navbar.component.js";
+import HeroCarousal from "../components/Hero.Carousal/HeroCarousal.component.js";
+
 const DefaultLayout = (props) => {
   return (
     <div>
       <Navbar/>
+      <HeroCarousal />
       {props.children}
     </div>
   );
