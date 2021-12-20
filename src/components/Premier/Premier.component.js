d5c2a1eca94b5f03b5c3c5199f55ba276bec4f67
Sanskruti3082 <sans.ullagaddi3082@gmail.com>
2021-12-20 18:10:01 +0530

    Premier

--- src/components/Premier/Premier.component.js
+++ src/components/Premier/Premier.component.js
@@ -17,53 +17,51 @@ const PremierImages =[
     src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302306-wsyqpgjqdc-portrait.jpg",
     alt:"Premier Image",
     title:"Proximity",
-    subtitle:"English",
-    isDark: false
+    subtitle:"English"
   },
   {
     src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302306-wsyqpgjqdc-portrait.jpg",
     alt:"Premier Image",
     title:"Proximity",
-    subtitle:"English",
-    isDark: false
+    subtitle:"English"
   },
   {
     src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302306-wsyqpgjqdc-portrait.jpg",
     alt:"Premier Image",
     title:"Proximity",
-    subtitle:"English",
-    isDark: false
+    subtitle:"English"
   },
   {
     src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302306-wsyqpgjqdc-portrait.jpg",
     alt:"Premier Image",
     title:"Proximity",
-    subtitle:"English",
-    isDark: false
+    subtitle:"English"
   },
   {
     src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302306-wsyqpgjqdc-portrait.jpg",
     alt:"Premier Image",
     title:"Proximity",
-    subtitle:"English",
-    isDark: false
+    subtitle:"English"
   },
   {
     src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302306-wsyqpgjqdc-portrait.jpg",
     alt:"Premier Image",
     title:"Proximity",
-    subtitle:"English",
-    isDark: false
+    subtitle:"English"
   }
 ];
   return (
     <>
+    <div className="flex flex-col items-start gap-1 py-4">
+     <h3 className="text-white text-xl font-bold">Premieres</h3>
+     <p className="text-white text-sm">Brand New releases every friday</p>
+    </div>
      <Slider {...settings} >
         {PremierImages.map((image)=> (
-          <Poster {...image}/>
+          <Poster {...image} isDark/>
         ))}
        </Slider>
-    </>
+       </>
   );
 };
 
